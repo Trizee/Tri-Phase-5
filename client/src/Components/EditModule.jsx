69cf11c397b33772444d6a45e00d08938e2b8322
Tri Hoang <trihoang09@gmail.com>
2023-10-26 09:06:45 -0600

    completed lab

---
+++ client/src/Components/EditModule.jsx
@@ -0,0 +1,36 @@
+import Editor from '@monaco-editor/react'
+import * as Y from "yjs"
+import { WebrtcProvider } from "y-webrtc"
+import { MonacoBinding } from "y-monaco"
+import { useRef } from 'react'
+
+function EditorModule({value,set,room,lang}){
+
+    const editorRef = useRef(null)
+
+    function handleEditorDidMount(editor){
+
+        editorRef.current = editor
+    
+        const doc = new Y.Doc()
+        
+        const provider = new WebrtcProvider(`${room}`,doc)
+        const type = doc.getText(`${room}`)
+        
+        const binding = new MonacoBinding(type, editorRef.current.getModel(), new Set([editorRef.current]), provider.awareness)
+      }
+
+    return (
+        <Editor 
+      height='25vh'
+      width='50vw'
+      value={value}
+      theme="vs-dark"
+      language={lang}
+      onMount={handleEditorDidMount}
+      onChange={set}
+    />
+    )
+}
+
+export default EditorModule
---
+++ client/src/Components/Editor.jsx
@@ -0,0 +1,58 @@
+import { useState, useEffect } from "react";
+import EditorModule from "./EditModule";
+
+function EditorComponent(){
+
+  const [html,setHtml] = useState('')
+  const [css,setCss] = useState('')
+  const [js,setJs] = useState('')
+  const [srcDoc,setSrcDoc] = useState('')
+
+  useEffect(()=>{
+    const timeout = setTimeout(()=>{
+      setSrcDoc(`
+        <html>
+          <body>${html}</body>
+          <style>${css}</style>
+          <script>${js}</script>
+        </html>
+      `)
+    },250)
+
+    return () => clearTimeout(timeout)
+
+  },[html,css,js])
+
+  return (
+    <>
+    <EditorModule 
+    value={html}
+    set={setHtml}
+    lang={'html'}
+    room={'html'}
+    />
+    <EditorModule 
+    value={css}
+    set={setCss}
+    lang={'css'}
+    room={'css'}
+    />
+    <EditorModule 
+    value={js}
+    set={setJs}
+    lang={'javascript'}
+    room={'javascript'}
+    />
+    <div>
+      <iframe 
+      srcDoc = {srcDoc}
+      title="output"
+      sandbox="allow-scripts"
+      width= '100%'
+      />
+    </div>
+  </>
+  )
+}
+
+export default EditorComponent
