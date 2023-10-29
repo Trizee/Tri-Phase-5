7ec9649c7cb15b0a722f933224452521053d6f7d
Tri <trihoang09@gmail.com>
2023-10-28 18:45:27 -0600

    added seperate windows for the editor

--- client/src/Editor/Editor.jsx
+++ client/src/Editor/Editor.jsx
@@ -7,7 +7,28 @@ function EditorComponent(){
   const [css,setCss] = useState('')
   const [js,setJs] = useState('')
   const [srcDoc,setSrcDoc] = useState('')
+  
+  // Making useStates for switcher function
+  const [active,setActive] = useState('')
+  const [editor,setEditor] = useState(null)
 
+  useEffect(()=>{
+    setActive('html')
+    setEditor(<EditorModule value={html} set={setHtml} lang={'html'} room={'html'}/>)
+  },[])
+
+  useEffect(()=>{
+    if (active === 'css'){
+      setEditor(<EditorModule value={css} set={setCss} lang={'css'} room={'css'} />)
+    }
+    else if(active ==='js'){
+      setEditor(<EditorModule value={js} set={setJs} lang={'javascript'} room={'javascript'}/>)
+    }
+    else {
+      setEditor(<EditorModule value={html} set={setHtml} lang={'html'} room={'html'}/>)
+    }
+  },[active])
+  
   useEffect(()=>{
     const timeout = setTimeout(()=>{
       setSrcDoc(`
@@ -23,32 +44,23 @@ function EditorComponent(){
 
   },[html,css,js])
 
+
+  // tab tab-bordered tab-active tab tab-bordered
+
   return (
     <>
     <div class="grid grid-cols-2">
-    <div >
-    <EditorModule 
-    value={html}
-    set={setHtml}
-    lang={'html'}
-    room={'html'}
-    />
-    <EditorModule 
-    value={css}
-    set={setCss}
-    lang={'css'}
-    room={'css'}
-    />
-    <EditorModule 
-    value={js}
-    set={setJs}
-    lang={'javascript'}
-    room={'javascript'}
-    />
+    <div>
+    <ul class="menu xl:menu-horizontal lg:min-w-max bg-base-300 w-full flex justify-center">
+        <a className={active === 'html'?'tab tab-bordered tab-active':'tab tab-bordered'} onClick={()=>setActive('html')}>HTML</a> 
+        <a className={active === 'css'?'tab tab-bordered tab-active':'tab tab-bordered'} onClick={()=>setActive('css')}>CSS</a> 
+        <a className={active === 'js'?'tab tab-bordered tab-active':'tab tab-bordered'} onClick={()=>setActive('js')}>JAVASCRIPT</a>
+    </ul>
+    {editor}
     </div>
     <div className="mockup-browser bg-base-300 rounded-none">
     <div class="mockup-browser-toolbar">
-    <div class="input">YourWebsite.com</div>
+    <div class="inpu">YourWebsite.com</div>
     </div>
     <iframe 
     srcDoc = {srcDoc}
