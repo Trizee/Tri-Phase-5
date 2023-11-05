f6a2e906aa281d16b06861ebd5f285fa43257a1a
Tri <trihoang09@gmail.com>
2023-11-05 15:52:00 -0700

    added search for dashboard

--- client/src/Dash/DashContainer.jsx
+++ client/src/Dash/DashContainer.jsx
@@ -1,8 +1,11 @@
 import ProjectForm from "./ProjectForm"
 import ProjectCard from "./ProjectCard"
 import { toast } from "react-toastify";
+import { useState } from "react";
 
 function DashContainer({setProjects,projects,user}){
+
+    const [search,setSearch] = useState('')
 
     function notifyE(string){
       toast.error(string, {
@@ -16,6 +19,10 @@ function DashContainer({setProjects,projects,user}){
         theme: "dark",
         });
     }
+
+    const projectDisplay = projects.filter(project => {
+      return project.title.toLowerCase().includes(search.toLowerCase())
+    })
 
     function notifyS(string){
       toast.success(string, {
@@ -51,18 +58,26 @@ function DashContainer({setProjects,projects,user}){
     return(
         <div className="h-full bg-purple-800 p-1">
           <div className="mx-auto mt-2 max-w-7xl px-4 py-6 sm:px-6 lg:px-8 rounded-lg bg-base-100 shadow-xl">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-300">Dashboard</h1>
+            <h1 className="text-3xl font-bold tracking-tight text-gray-300 text-center">Dashboard</h1>
           </div>
         <main>
         <div className="m-auto max-w-7xl mt-2 mb-2 shadow-xl">
-        <div className="mx-auto mt-2 max-w-7xl px-4 py-6 sm:px-6 lg:px-8 rounded-lg bg-base-100 shadow-xl">
-            <h1 className="text-2xl font-bold tracking-tight text-gray-300 pb-4">Your Projects</h1>
-            <div className="flex justify-left mx-0 md:mx-12 h-42 overflow-auto">
-            {projects.map((project)=>(
+        <div className="mx-auto mt-2 rounded-lg bg-base-100 shadow-xl">
+        <div className="mx-auto mt-2 max-w-7xl px-4 py-6 sm:px-6 lg:px-8 rounded-lg shadow-xl flex">
+            <h1 className="text-3xl font-bold tracking-tight text-gray-300">Your Projects</h1>
+            
+            <div className="form-control ml-auto mr-0 md:mr-6">
+              <input type="text" placeholder="Search" onChange={(e)=>setSearch(e.target.value)} className="input input-bordered w-24 md:w-auto focus:bg-gray-900" />
+            </div>
+
+            <svg xmlns="http://www.w3.org/2000/svg" className="mt-2 h-8 w-8 hover:stroke-gray-200" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+        </div> 
+            <div className="grid grid-cols-2 md:grid-cols-4 lg:grid-cols-6 p-0 md:p-6 h">
+            {projectDisplay.map((project)=>(
               <ProjectCard key={project.id} project={project} leftFunc={deleteProject} user={user} set={null}/>
             ))}
             </div>
-        </div>
+            </div>
             {/* Your content */}
             <ProjectForm projects={projects} setProjects={setProjects}/>
         </div>
